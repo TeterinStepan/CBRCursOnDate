892fdfa5846ebb0d335626b4c93621a776d7b159
Stepan Teterin <37660781+TeterinStepan@users.noreply.github.com>
2022-07-09 01:20:06 +0500

    Update DateViewer.tsx

--- src/modules/cursOnDate/previews/cursOnDateStatus/DateViewer.tsx
+++ src/modules/cursOnDate/previews/cursOnDateStatus/DateViewer.tsx
@@ -1,13 +1,17 @@
 import {memo} from "react";
 import Paragraph from "../../../userInterface/typography/Paragraph";
-import {getDateFormatted} from "../../../dates/DatesLib";
+import {getDateFormatted, getTimeFormatted} from "../../../dates/DatesLib";
+import {View} from "react-native";
 
 type DateViewerType = {
     date: Date
 }
 const DateViewer = ({date}: DateViewerType) => {
     return (
-        <Paragraph>Date: {getDateFormatted(date)}</Paragraph>
+        <View style={{flexDirection:'row', justifyContent:'space-between'}}>
+            <Paragraph compact>Date: {getDateFormatted(date)}</Paragraph>
+            <Paragraph compact>Last update: {getTimeFormatted(date)}</Paragraph>
+        </View>
     )
 }
 
